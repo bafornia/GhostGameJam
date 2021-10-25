d3c095ed1a76def20747d8b3711309399369e70d
Brevillo <oliver.beebe@digipen.edu>
2021-10-25 14:41:10 -0700

    basic ghost AI complete
    ghost AI can now follow the player then return to a sentry station if it loses sight of the player or they get to far away.

--- Assets/Scripts/GhostAI.cs
+++ Assets/Scripts/GhostAI.cs
@@ -9,22 +9,52 @@ public class GhostAI : MonoBehaviour
     [Tooltip("The speed the ghost moves up and down for the floating animation.")]
     public float floatingPeriod = 1.0f;
 
+    [Tooltip("The player the ghost will chase. Defaults to whatever game object is called player")]
+    public GameObject player;
+
+    [Tooltip("Speed of the ghost while chasing the player.")]
+    public float chaseSpeed = 3;
+    [Tooltip("Speed of the ghost while returning to its sentry position.")]
+    public float returnSpeed = 2;
+    [Tooltip("Horizontal distance from which the ghost can see the player.")]
+    public float xChaseRange = 5;
+    [Tooltip("Vertical range from which the ghost can see the player.")]
+    public float yChaseRange = 3;
+    [Tooltip("How far the ghost will chase the player from its starting point before it gives up.")]
+    public float giveUpRange = 10;
+
     float floatingAnimation = 0;
     float floatingPosition;
-
-    Vector3 GhostPosition;
+    
+    Vector3 sentryPosition, ghostPosition;
 
     void Start()
     {
-        GhostPosition = transform.position;
+        if (player == null)
+        {
+            player = GameObject.Find("player");
+        }
+
+        sentryPosition = ghostPosition = transform.position;
     }
 
     private void FixedUpdate()
     {
+        // floating animation
         floatingAnimation += 50 * Time.fixedDeltaTime;
-
         floatingPosition = floatingAmplitude / 10 * Mathf.Sin(floatingPeriod / 100 * floatingAnimation);
 
-        transform.position = new Vector3(GhostPosition.x, GhostPosition.y + floatingPosition, 0);
+        // chasing AI
+        if ((player.transform.position - transform.position).sqrMagnitude <= new Vector3(xChaseRange, yChaseRange, 0).sqrMagnitude
+            && (transform.position - sentryPosition).sqrMagnitude <= giveUpRange * giveUpRange)
+        {
+            ghostPosition += chaseSpeed * (player.transform.position - ghostPosition).normalized * Time.fixedDeltaTime;
+        }
+        else if((ghostPosition - sentryPosition).sqrMagnitude >= 1f)
+        {
+            ghostPosition += returnSpeed * (sentryPosition - ghostPosition).normalized * Time.fixedDeltaTime;
+        }
+
+        transform.position = new Vector3(ghostPosition.x, ghostPosition.y + floatingPosition, ghostPosition.z);
     }
 }
